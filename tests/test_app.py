c540f1ba0e8aa6e5cb740acbb64ec01f26580aee
Cristiano Muroni <muronidev@gmail.com>
2025-05-14 14:28:17 -0300

    Update pyproject file and test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,15 @@
-from fastapi.testclient import TestClient # type: ignore
 from http import HTTPStatus
 
+from fastapi.testclient import TestClient  # type: ignore
 
 from fastapi_zero.app import app
 
+
 def test_root_deve_retornar_ola_mundo():
-    #Arrange
-    client = TestClient(app) 
-    #Act
+    # Arrange
+    client = TestClient(app)
+    # Act
     response = client.get('/')
-    #Assert
+    # Assert
     assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
